b8493858e1f6b748214f7423c6ce0e168531f023
nguyentuandung <nguyentunadung23012003@gmail.com>
2024-06-01 19:11:52 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from ultralytics import settings
+from ultralytics import YOLO
+import torch
+import faulthandler
+
+# settings.update({"runs_dir": "C:\\Users\\Tuand\\PycharmProjects\\YOLO\\runs", "weights_dir": "C:\\Users\\Tuand\\PycharmProjects\\YOLO\\weights", "datasets_dir": "C:\\Users\\Tuand\\PycharmProjects\\YOLO\\datasets"})
+# if torch.cuda.is_available():
+#     device = "0"
+# else:
+#     device = "cpu"
+#
+# if device == "0":
+#     torch.cuda.set_device(0)
+# print(device)
+
+
+if __name__ == "__main__":
+    faulthandler.enable()  # start @ the beginning
+    model = YOLO("C:\\Users\\Tuand\\PycharmProjects\\YOLO\\runs\\detect\\train8\\weights\\last.pt")
+    results = model.train(data="wider.yaml", epochs=5, batch=3, imgsz=640, save_period=5, device=[0], amp=False, resume=True)
